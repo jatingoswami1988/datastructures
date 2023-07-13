a682d49eb85a08a68d825496cc0c6876a703190d
INFOCORP\jatin.goswami <jatin.goswami@ihsmarkit.com>
2023-07-13 17:43:09 +0530

    Changes

--- src/com/fis/Testing.java
+++ src/com/fis/Testing.java
@@ -3,10 +3,6 @@ package com.fis;
 public class Testing {
 
 	public static void main(String ars[]) {
-
-		// int arr[] = { 10, 1, 3, 1, 2, 2, 1, 0, 4 };
-		// int arr[] = { 5, 3, 1, 3, 2, 3 };
-		// int arr[] = { 9, 9, 9, 9, 9 };
 		int arr[] = { 1, 5, 2, 4, 3, 3 };
 		int finalCount = 0;
 		for (int i = 0; i < arr.length; i++) {	
